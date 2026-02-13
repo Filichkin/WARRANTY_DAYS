6b303faff5fccb8c8ff806bb054e22e035485ff9
Alexey Filichkin <filichkin.alexey@gmail.com>
2026-02-13 21:58:30 +0300

    upd RoOpenDate  time.Time

--- internal/models/claims.go
+++ internal/models/claims.go
@@ -7,7 +7,7 @@ type Claim struct {
 	VIN         string     `json:"vin" gorm:"column:vin;not null;index"`
 	RetailDate  time.Time  `json:"retail_date" gorm:"column:retail_date;type:date;not null"`
 	RoOpenDate  time.Time  `json:"ro_open_date" gorm:"column:ro_open_date;type:date;not null"`
-	RoCloseDate time.Time `json:"ro_close_date" gorm:"column:ro_close_date;type:date;not null"`
+	RoCloseDate time.Time  `json:"ro_close_date" gorm:"column:ro_close_date;type:date;not null"`
 	CreatedAt   time.Time  `json:"created_at" gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt   time.Time  `json:"updated_at" gorm:"column:updated_at;autoUpdateTime"`
 }
